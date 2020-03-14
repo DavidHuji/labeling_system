5deb7bb755aecbc2d4879568b1b0ab285963137f
DavidHuji <David.nukrai@mail.huji.ac.il>
2020-03-14 23:51:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ from pathlib import Path
 
 LABELS = ['1', '2', '3', '4']
 
+
+print(f'inputs are:')
+
 print(f'inputs are: {sys.argv[1:]}')
-input_folder, outputs_folder, patch_size, resize_factor = sys.argv[1:]
+input_folder, outputs_folder, patch_size, resize_factor = sys.argv[1:5]
 if not os.path.exists(input_folder):
     print(f'The following input path not exists: {input_folder}')
     exit(0)
@@ -28,7 +31,9 @@ resize_factor = int(resize_factor)
 patch_size = int(patch_size)
 
 in_imgs_paths_list = [os.path.join(input_folder, o) for o in os.listdir(input_folder)
-                      if (not os.path.isdir(os.path.join(input_folder, o))) and (o[-3:] == 'jpg' or o[-3:] == 'png' or o[-3:] == 'PNG')]
+                      if (not os.path.isdir(os.path.join(input_folder, o))) and (o[-3:] == 'jpg' or o[-3:] == 'png' or o[-3:] == 'PNG' or o[-3:] == 'npz')]
+
+
 patches = []
 cur_patch = []
 rand_x, rand_y, cur_path, img_path = 0, 0, 0, 0
@@ -47,12 +52,21 @@ def press(event):
 
 while True:
     img_path = in_imgs_paths_list[random.randint(0, len(in_imgs_paths_list) - 1)]
-    img = plt.imread(img_path)
+    try:
+        img = plt.imread(img_path)
+    except:
+        data = np.load(img_path)
+        try:
+            img = data['data'][int(sys.argv[5])]
+        except:
+            print(f'\n\nIllegal input for channel index')
+            break
     shape = np.array(img).shape
+    print(shape)
     rand_x, rand_y = random.randint(patch_size, shape[0] - patch_size), random.randint(patch_size, shape[1] - patch_size)
     fig, ax = plt.subplots()
     fig.canvas.mpl_connect('key_press_event', press)
     cur_patch = img[rand_x:rand_x + patch_size, rand_y:rand_y + patch_size]
     cur_patch = cv2.resize(cur_patch, (0, 0), fx=1 / resize_factor, fy=1 / resize_factor, interpolation=cv2.INTER_LINEAR)
-    ax.imshow(cur_patch)
+    ax.imshow(cur_patch, cmap='gray')
     plt.show()
